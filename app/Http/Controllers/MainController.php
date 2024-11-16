a2d668061cd61f07fe9f2fce83516dc06099c853
Vladimir Musatov <vovamusatov.2001@gmail.com>
2024-11-16 19:47:37 +0200

    user seeder

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Position;
+use App\Models\User;
 
 
 class MainController extends Controller
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Faker\Factory as Faker;
 
 use App\Models\Position;
 use App\Models\User;
@@ -18,13 +17,48 @@ class UserSeeder extends Seeder
     public function run(): void
     {
 
-        $faker = Faker::create();
+        $curl = curl_init();
 
-        for($i = 0 ;  $i < 45; $i++)
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://randomuser.me/api/1.4?nat=ua&results=15&inc=name,email,phone,picture',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+          ));
+
+          
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+
+        $positions_ids = Position::pluck('id');
+
+        $response = json_decode($response);
+
+        $api_result = collect($response)->first();
+
+        $users = [];
+
+        foreach($api_result as $item)
         {
-            User::create([
+            $phone = $item->phone;
+            $phone = preg_replace('/\D/', '', $phone); 
+            $phone = '+38' . $phone;
 
-            ]);    
+
+            $users[] = [
+                'name' => $item->name->first .' ' . $item->name->last,
+                'email' => $item->email,
+                'photo' => $item->picture->medium,
+                'phone' => $phone,
+                'position_id' => $positions_ids->random(),
+            ];
         }
+
+        User::insert($users);
     }
 }
